35858b46d7ad44e7e41052c248b3432e3524fa08
TrueBallin <ballin_true@yahoo.com>
2020-01-27 20:40:48 -0800

    Number of vowels

--- numberOfVowels.playground/Contents.swift
+++ numberOfVowels.playground/Contents.swift
@@ -1,5 +1,7 @@
 import UIKit
 
 func numberOfVowels(in string: String, isYAVowel: Bool = false) -> Int {
-    
+    let vowel = ["A,E,I,O,U,Y"]
+    var numberOfVowels = 0
+    if numberOfVowels =
 }
